1af258e59a2c8c34a20cb04367043df2bd2a73a4
Seokjun7074 <sukjune96@gmail.com>
2023-09-11 15:48:46 +0900

    ✨ feat: 전역 스타일링 설정

---
+++ client/src/styles/theme.ts
@@ -0,0 +1,16 @@
+const color = {
+  black: 'black',
+  white: 'white',
+};
+
+const fontSize = {
+  s: '1rem',
+  m: '1.6rem',
+  l: '2.2rem',
+  xl: '3rem',
+};
+
+export const theme = {
+  color,
+  fontSize,
+};
