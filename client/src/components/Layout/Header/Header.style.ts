5a48204ef0a72303b258950982a1e1336bb3c43a
Seokjun7074 <sukjune96@gmail.com>
2023-09-11 17:23:45 +0900

    ✨ feat: Header컴포넌트 세팅

---
+++ client/src/components/Layout/Header/Header.style.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const HeaderWrapper = styled.header`
+  width: 100%;
+  height: 6rem;
+  padding: 0.5rem;
+  display: flex;
+  justify-content: space-between;
+`;
