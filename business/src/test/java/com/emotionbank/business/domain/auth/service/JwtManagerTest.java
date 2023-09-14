83ebbd7461a9b2844ad9d0186dc37406f311e9bd
양정훈 <yjhoon3658@naver.com>
2023-09-14 15:37:30 +0900

    :white_check_mark: test: accessToken, RefreshToken이 모두 유효한지 검증하는 테스트 코드 작성

--- business/src/test/java/com/emotionbank/business/domain/auth/service/JwtManagerTest.java
+++ business/src/test/java/com/emotionbank/business/domain/auth/service/JwtManagerTest.java
@@ -1,6 +1,7 @@
 package com.emotionbank.business.domain.auth.service;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,16 +16,30 @@ public class JwtManagerTest {
 	@Autowired
 	JwtManager jwtManager;
 
-	@Test
-	@DisplayName("jwt 토큰을 생성하는 테스트")
-	public void createJwtToken() {
-		JwtTokens jwtTokens = jwtManager.createJwtTokens(1L);
+	private static final Long SAMPLE_USER_ID = 1L;
 
+	@Test
+	@DisplayName("jwt 토큰 생성을 성공한다.")
+	public void createJwtToken() {
+		// given
+		JwtTokens jwtTokens = jwtManager.createJwtTokens(SAMPLE_USER_ID);
+
+		// when & then
 		assertThat(jwtTokens.getGrantType()).isNotBlank();
 		assertThat(jwtTokens.getAccessToken()).isNotBlank();
 		assertThat(jwtTokens.getRefreshToken()).isNotBlank();
 		assertThat(jwtTokens.getAccessTokenExpirationTime()).isNotNull();
 		assertThat(jwtTokens.getRefreshTokenExpirationTime()).isNotNull();
+	}
+
+	@Test
+	@DisplayName("accessToken과 refreshToken이 모두 유효한 로직일때 성공한다.")
+	public void validateTokenSuccess() {
+		// given
+		final JwtTokens jwtTokens = jwtManager.createJwtTokens(SAMPLE_USER_ID);
+
+		// when & then
+		assertDoesNotThrow(() -> jwtManager.validateTokens(jwtTokens));
 	}
 
 }
