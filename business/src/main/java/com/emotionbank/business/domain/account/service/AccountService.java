f962ddb44658bc5d08c0a0c170442df39c2c5440
jxixeun <jxixeun@gmail.com>
2023-09-15 17:05:59 +0900

    :sparkles: feat: AccountService에 계좌 생성 메서드 선언

--- business/src/main/java/com/emotionbank/business/domain/account/service/AccountService.java
+++ business/src/main/java/com/emotionbank/business/domain/account/service/AccountService.java
@@ -1,4 +1,8 @@
 package com.emotionbank.business.domain.account.service;
 
+import com.emotionbank.business.domain.account.dto.AccountDto;
+
 public interface AccountService {
+
+	AccountDto createAccount(Long userId, String accountName);
 }
