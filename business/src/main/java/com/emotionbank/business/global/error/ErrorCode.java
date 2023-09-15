71cb3550cce107acd3f628033332f89449834b4b
jxixeun <jxixeun@gmail.com>
2023-09-15 17:06:31 +0900

    :sparkles: feat: 존재하지 않는 회원에 대한 ErrorCode Enum 생성

--- business/src/main/java/com/emotionbank/business/global/error/ErrorCode.java
+++ business/src/main/java/com/emotionbank/business/global/error/ErrorCode.java
@@ -11,7 +11,9 @@ public enum ErrorCode {
 	REFRESH_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "J-001", "Refresh Token 이 만료되었습니다."),
 	REFRESH_TOKEN_INVALID(HttpStatus.UNAUTHORIZED, "J-002", "유효하지 않은 Refresh Token 입니다."),
 	ACCESS_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "J-003", "Access Token 이 만료되었습니다."),
-	ACCESS_TOKEN_INVALID(HttpStatus.UNAUTHORIZED, "J-004", "유효하지 않은 Access Token 입니다.");
+	ACCESS_TOKEN_INVALID(HttpStatus.UNAUTHORIZED, "J-004", "유효하지 않은 Access Token 입니다."),
+	// User
+	USER_NOT_FOUND(HttpStatus.NOT_FOUND,"U-001","존재하지 않는 회원입니다.");
 
 	ErrorCode(HttpStatus httpStatus, String errorCode, String message) {
 		this.httpStatus = httpStatus;
