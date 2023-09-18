bf98174020bd02b01bfbea0a3b577ca427ed6d92
yerimstar <yerimhuh@naver.com>
2023-09-18 15:37:44 +0900

    :sparkles: feat: 입/출금 API 구현

--- business/src/main/java/com/emotionbank/business/api/transaction/controller/TransactionController.java
+++ business/src/main/java/com/emotionbank/business/api/transaction/controller/TransactionController.java
@@ -1,4 +1,29 @@
 package com.emotionbank.business.api.transaction.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.emotionbank.business.api.transaction.dto.UpdateBalanceDto;
+import com.emotionbank.business.domain.transaction.dto.TransactionDto;
+import com.emotionbank.business.domain.transaction.service.TransactionService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/transactions")
 public class TransactionController {
+
+	private final TransactionService transactionService;
+
+	@PostMapping
+	public ResponseEntity<UpdateBalanceDto.Response> deposit(
+		@RequestBody UpdateBalanceDto.Request request) {
+		TransactionDto transactionDto = transactionService.updateBalance(TransactionDto.from(request));
+		return ResponseEntity.ok(UpdateBalanceDto.Response.from(transactionDto));
+	}
+
 }
