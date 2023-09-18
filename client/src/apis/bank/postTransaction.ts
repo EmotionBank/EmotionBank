908f30bb79b1b93e020fa97889f83d34fe97f33c
Seokjun7074 <sukjune96@gmail.com>
2023-09-18 13:44:10 +0900

    ✏️ fix: 본인 계좌 입/출금 API 변경

--- client/src/apis/bank/postTransaction.ts
+++ client/src/apis/bank/postTransaction.ts
@@ -1,13 +1,10 @@
 import { axiosInstance } from '@/apis';
 import { DepositTransactionType } from '@/types/bank';
 
-// 계좌 입금 (본인에게 +/-)
+// 계좌 입출금 (본인에게 +/-)
 export interface PostDepositTransaction {
   transactionType: string;
   categoryId: string;
-  title: string;
-  sender: string;
-  receiver: string;
   amount: number;
   balance: number;
   emotion: string;
@@ -15,7 +12,7 @@ export interface PostDepositTransaction {
   visibility: string;
   transactionDate: Date;
 }
-export const depositTransaction = async (transactionData: PostDepositTransaction) => {
-  const { data } = await axiosInstance.post<DepositTransactionType>('/accounts/deposit', transactionData);
+export const postTransaction = async (transactionData: PostDepositTransaction) => {
+  const { data } = await axiosInstance.post<DepositTransactionType>('/transactions', transactionData);
   return data;
 };
