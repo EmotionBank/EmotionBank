7d6bce879ace5ddd1d81999933c463375741e339
jxixeun <jxixeun@gmail.com>
2023-09-18 18:15:54 +0900

    :sparkles: feat: 계좌명 수정 API 구현

--- business/src/main/java/com/emotionbank/business/api/account/controller/AccountController.java
+++ business/src/main/java/com/emotionbank/business/api/account/controller/AccountController.java
@@ -2,11 +2,13 @@ package com.emotionbank.business.api.account.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.emotionbank.business.api.account.dto.CheckAccountBalanceDto;
+import com.emotionbank.business.api.account.dto.UpdateAccountDto;
 import com.emotionbank.business.domain.account.dto.AccountDto;
 import com.emotionbank.business.domain.account.service.AccountService;
 
@@ -22,5 +24,11 @@ public class AccountController {
 	public ResponseEntity<CheckAccountBalanceDto.Response> getAccountBalance(@RequestBody CheckAccountBalanceDto.Request request) {
 		AccountDto accountDto = accountService.getAccountBalance(request.getAccountNumber());
 		return ResponseEntity.ok(CheckAccountBalanceDto.Response.from(accountDto));
+	}
+
+	@PutMapping
+	public ResponseEntity<?> updateAccountName(@RequestBody UpdateAccountDto.Request request) {
+		accountService.updateAccountName(request.getAccountNumber(), request.getAccountName());
+		return ResponseEntity.ok().build();
 	}
 }
