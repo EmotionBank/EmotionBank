9c5e7693206a65baea82c72351cb3eee9a6c9079
yerimstar <yerimhuh@naver.com>
2023-09-19 15:49:26 +0900

    :sparkles: feat: 거래내역 조회 API 구현

--- business/src/main/java/com/emotionbank/business/api/transaction/controller/TransactionController.java
+++ business/src/main/java/com/emotionbank/business/api/transaction/controller/TransactionController.java
@@ -1,13 +1,18 @@
 package com.emotionbank.business.api.transaction.controller;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.emotionbank.business.api.transaction.dto.GetTransactionListDto;
 import com.emotionbank.business.api.transaction.dto.UpdateBalanceDto;
 import com.emotionbank.business.domain.transaction.dto.TransactionDto;
+import com.emotionbank.business.domain.transaction.dto.TransactionSearchDto;
 import com.emotionbank.business.domain.transaction.service.TransactionService;
 
 import lombok.RequiredArgsConstructor;
@@ -26,4 +31,10 @@ public class TransactionController {
 		return ResponseEntity.ok(UpdateBalanceDto.Response.from(transactionDto));
 	}
 
+	@GetMapping
+	public ResponseEntity<GetTransactionListDto.Response> getTransactions(
+		@RequestBody GetTransactionListDto.Request request) {
+		List<TransactionDto> transactionList = transactionService.getTransactions(TransactionSearchDto.from(request));
+		return ResponseEntity.ok(GetTransactionListDto.Response.from(transactionList));
+	}
 }
