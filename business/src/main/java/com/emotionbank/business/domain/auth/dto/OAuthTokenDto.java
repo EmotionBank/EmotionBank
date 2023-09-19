73b32c341bfe8a44923b78143f16d2736a9cc179
양정훈 <yjhoon3658@naver.com>
2023-09-19 09:54:28 +0900

    :sparkles: feat: Response에 refreshToken 추가

--- business/src/main/java/com/emotionbank/business/domain/auth/dto/OAuthTokenDto.java
+++ business/src/main/java/com/emotionbank/business/domain/auth/dto/OAuthTokenDto.java
@@ -10,12 +10,14 @@ import lombok.NoArgsConstructor;
 
 public class OAuthTokenDto {
 	@Getter
+	@NoArgsConstructor(access = AccessLevel.PROTECTED)
+	@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 	public static class Request {
 		private final String grantType = "authorization_code";
-		private final String clientId;
-		private final String clientSecret;
-		private final String redirectUri;
-		private final String code;
+		private String clientId;
+		private String clientSecret;
+		private String redirectUri;
+		private String code;
 
 		@Builder
 		public Request(String clientId, String clientSecret, String redirectUri, String code) {
@@ -42,13 +44,18 @@ public class OAuthTokenDto {
 		private String tokenType;
 		private String accessToken;
 		private Integer expiresIn;
+		private String refreshToken;
+		private Integer refreshTokenExpiresIn;
 		private String scope;
 
 		@Builder
-		public Response(String tokenType, String accessToken, Integer expiresIn, String scope) {
+		public Response(String tokenType, String accessToken, Integer expiresIn, String refreshToken,
+			Integer refreshTokenExpiresIn, String scope) {
 			this.tokenType = tokenType;
 			this.accessToken = accessToken;
 			this.expiresIn = expiresIn;
+			this.refreshToken = refreshToken;
+			this.refreshTokenExpiresIn = refreshTokenExpiresIn;
 			this.scope = scope;
 		}
 	}
