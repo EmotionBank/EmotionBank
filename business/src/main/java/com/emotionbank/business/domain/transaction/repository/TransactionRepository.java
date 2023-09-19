9b89ce64efe320e8cab3ec2e51725642d3d25687
yerimstar <yerimhuh@naver.com>
2023-09-19 15:49:38 +0900

    :sparkles: feat: 계좌번호와 날짜로 거래내역 조회

--- business/src/main/java/com/emotionbank/business/domain/transaction/repository/TransactionRepository.java
+++ business/src/main/java/com/emotionbank/business/domain/transaction/repository/TransactionRepository.java
@@ -1,8 +1,15 @@
 package com.emotionbank.business.domain.transaction.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.sql.Date;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.emotionbank.business.domain.account.entity.Account;
 import com.emotionbank.business.domain.transaction.entity.Transaction;
 
-public interface TransactionRepository extends JpaRepository<Transaction,Long> {
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+	@Query("SELECT t FROM Transaction t WHERE (t.sender = :account OR t.receiver = :account) AND date(t.transactionTime) = :date")
+	List<Transaction> searchByAccountAndDate(Account account, Date date);
 }
