969dfc53188f4415b19c5816e83639eebe40aaab
양정훈 <yjhoon3658@naver.com>
2023-09-19 09:56:58 +0900

    :sparkles: feat: kakao 에서 유저 정보를 불러올 때 필요한 field 값 추가

--- business/src/main/java/com/emotionbank/business/domain/auth/kakao/dto/KakaoInfoResponseDto.java
+++ business/src/main/java/com/emotionbank/business/domain/auth/kakao/dto/KakaoInfoResponseDto.java
@@ -1,11 +1,39 @@
 package com.emotionbank.business.domain.auth.kakao.dto;
 
+import java.time.LocalDateTime;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Getter
+@Setter
 public class KakaoInfoResponseDto {
 	@JsonProperty("id")
 	private String kakaoId;
+
+	@JsonProperty("kakao_account")
+	private KakaoAccount kakaoAccount;
+
+	@JsonProperty("connected_at")
+	private LocalDateTime connectedAt;
+
+	@Getter
+	@Setter
+	@ToString
+	public static class KakaoAccount {
+
+		private Profile profile;
+
+		@Getter
+		@Setter
+		@ToString
+		public static class Profile {
+
+			private String nickname;
+		}
+
+	}
 }
