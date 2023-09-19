ba8d48a976fb4726d69521490dfce21ca0711f78
yerimstar <yerimhuh@naver.com>
2023-09-19 15:49:52 +0900

    :sparkles: feat: 계좌번호와 날짜로 거래내역 조회하는 메서드 구현

--- business/src/main/java/com/emotionbank/business/domain/transaction/service/TransactionService.java
+++ business/src/main/java/com/emotionbank/business/domain/transaction/service/TransactionService.java
@@ -1,9 +1,13 @@
 package com.emotionbank.business.domain.transaction.service;
 
+import java.util.List;
+
 import com.emotionbank.business.domain.transaction.dto.TransactionDto;
+import com.emotionbank.business.domain.transaction.dto.TransactionSearchDto;
 
 public interface TransactionService {
-	// 입금
 	TransactionDto updateBalance(TransactionDto transactionDto);
+
+	List<TransactionDto> getTransactions(TransactionSearchDto transactionSearchDto);
 
 }
