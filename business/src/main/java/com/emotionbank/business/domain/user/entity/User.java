985966023667ef9d9146cc5b106c646879d444d8
양정훈 <yjhoon3658@naver.com>
2023-09-19 09:20:31 +0900

    :sparkles: feat: User Entity 의 필드들에 옵션 추가 및 update 문 구현

--- business/src/main/java/com/emotionbank/business/domain/user/entity/User.java
+++ business/src/main/java/com/emotionbank/business/domain/user/entity/User.java
@@ -1,8 +1,10 @@
 package com.emotionbank.business.domain.user.entity;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -11,18 +13,17 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import org.springframework.data.annotation.CreatedDate;
+
 import com.emotionbank.business.domain.account.entity.Account;
 import com.emotionbank.business.domain.user.dto.Role;
 import com.emotionbank.business.domain.user.dto.SocialType;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Builder
-@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 public class User {
@@ -30,24 +31,29 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long userId;
 
+	@Column(nullable = false)
 	private String nickname;
 
-
+	@Column(nullable = false)
 	private LocalDate birthday;
 
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
 	private Role role;
 
-
+	@Column(nullable = false)
 	private String socialId;
 
+	@Column(nullable = false)
 	@Enumerated(EnumType.STRING)
 	private SocialType socialType;
 
-	private LocalDate createdTime;
+	@CreatedDate
+	private LocalDateTime createdTime;
 
-	private LocalDate lastLoginTime;
+	private LocalDateTime lastLoginTime;
 
-	private LocalDate withdrawalTime;
+	private LocalDateTime withdrawalTime;
 
 	@OneToMany(mappedBy = "user")
 	private List<Account> accounts;
@@ -59,6 +65,39 @@ public class User {
 	private List<Category> category;
 
 	private String image;
+
+	@Builder
+	public User(Long userId, String nickname, LocalDate birthday, Role role, String socialId, SocialType socialType,
+		LocalDateTime createdTime, LocalDateTime lastLoginTime, LocalDateTime withdrawalTime, String image) {
+		this.userId = userId;
+		this.nickname = nickname;
+		this.birthday = birthday;
+		this.role = role;
+		this.socialId = socialId;
+		this.socialType = socialType;
+		this.createdTime = createdTime;
+		this.lastLoginTime = lastLoginTime;
+		this.withdrawalTime = withdrawalTime;
+		this.image = image;
+	}
+
+	public static User of(String id, SocialType socialType) {
+		return User.builder()
+			.nickname("")
+			.birthday(LocalDate.now())
+			.role(Role.PENDING)
+			.socialId(id)
+			.socialType(socialType)
+			.build();
+	}
+
+	public void updateWithdrawalTime(LocalDateTime withdrawalTime) {
+		this.withdrawalTime = withdrawalTime;
+	}
+
+	public void updateLastLoginTime(LocalDateTime lastLoginTime) {
+		this.lastLoginTime = lastLoginTime;
+	}
 
 	// @OneToMany(mappedBy = "user")
 	// private List<Calendar> calendar;
