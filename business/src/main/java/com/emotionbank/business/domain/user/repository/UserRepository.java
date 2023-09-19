78d89b6b190978276734a450491090997dba699c
양정훈 <yjhoon3658@naver.com>
2023-09-19 09:22:29 +0900

    :sparkles: feat: SocialId 와 SocialType 을 이용해 User 를 찾는 메서드 추가

--- business/src/main/java/com/emotionbank/business/domain/user/repository/UserRepository.java
+++ business/src/main/java/com/emotionbank/business/domain/user/repository/UserRepository.java
@@ -7,9 +7,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.emotionbank.business.domain.user.dto.SocialType;
 import com.emotionbank.business.domain.user.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 	Optional<List<User>> findByNicknameContains(String nickname, Pageable pageable);
+
+	Optional<User> findBySocialIdAndSocialType(String socialId, SocialType socialType);
 }
