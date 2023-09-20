d2c2c28cbf284465b7be6ea838b1cbb6a91c0986
Seokjun7074 <sukjune96@gmail.com>
2023-09-20 13:37:27 +0900

    ✨ feat: 달력 이모지 조회 API 생성

---
+++ client/src/apis/bank/getCalendarData.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+export interface GetCalendar {
+  accountId: string;
+  date: string; // 2023-09
+}
+
+// 달력에 뿌릴 데이터
+export const getCalendarData = async (calendarParams: GetCalendar) => {
+  const { data } = await axios.get('/calendar', { params: { ...calendarParams } });
+  return data;
+};
