110db56323bcacfe8656b190b625375a9a76523a
jxixeun <jxixeun@gmail.com>
2023-09-20 13:07:06 +0900

    :sparkles: feat: WebConfig에 XSS 방어 설정(JSON)

--- business/src/main/java/com/emotionbank/business/global/config/WebConfig.java
+++ business/src/main/java/com/emotionbank/business/global/config/WebConfig.java
@@ -1,16 +1,27 @@
 package com.emotionbank.business.global.config;
 
+import java.util.List;
+
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.RequiredArgsConstructor;
 
 import com.navercorp.lucy.security.xss.servletfilter.XssEscapeServletFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+	private final ObjectMapper objectMapper;
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
@@ -32,5 +43,17 @@ public class WebConfig implements WebMvcConfigurer {
 		filterRegistration.setOrder(1);
 		filterRegistration.addUrlPatterns("/*");
 		return filterRegistration;
+	}
+
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.add(jsonEscapeConverter());
+	}
+
+	@Bean
+	public MappingJackson2HttpMessageConverter jsonEscapeConverter() {
+		ObjectMapper copy = objectMapper.copy();
+		copy.getFactory().setCharacterEscapes(new HtmlCharacterEscapes());
+		return new MappingJackson2HttpMessageConverter(copy);
 	}
 }
