43ab5258e0cf05a50762ee461c9d97cac848bb28
yerimstar <yerimhuh@naver.com>
2023-09-20 12:56:26 +0900

    :pencil2: fix: parameter를 각각 받는 of 메서드로 수정

--- business/src/main/java/com/emotionbank/business/domain/transaction/dto/TransactionSearchDto.java
+++ business/src/main/java/com/emotionbank/business/domain/transaction/dto/TransactionSearchDto.java
@@ -2,29 +2,28 @@ package com.emotionbank.business.domain.transaction.dto;
 
 import java.sql.Date;
 import java.time.LocalDate;
-import java.util.Map;
 
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class TransactionSearchDto {
-	private String accountNumber;
+	private Long accountId;
 	private Date startDate;
 	private Date endDate;
 
 	@Builder
-	public TransactionSearchDto(String accountNumber, Date startDate, Date endDate) {
-		this.accountNumber = accountNumber;
+	public TransactionSearchDto(Long accountId, Date startDate, Date endDate) {
+		this.accountId = accountId;
 		this.startDate = startDate;
 		this.endDate = endDate;
 	}
 
-	public static TransactionSearchDto from(Map<String, String> paramMap) {
+	public static TransactionSearchDto of(Long accountId, String startDate, String endDate) {
 		return TransactionSearchDto.builder()
-			.accountNumber(paramMap.get("accountNumber"))
-			.startDate(Date.valueOf(LocalDate.parse(paramMap.get("startDate"))))
-			.endDate(Date.valueOf(LocalDate.parse(paramMap.get("endDate"))))
+			.accountId(accountId)
+			.startDate(Date.valueOf(LocalDate.parse(startDate)))
+			.endDate(Date.valueOf(LocalDate.parse(endDate)))
 			.build();
 	}
 }
