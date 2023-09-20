d2681ebed7976fc50781a82e6c38480ceddcff9b
Seokjun7074 <sukjune96@gmail.com>
2023-09-20 12:53:31 +0900

    ✨ feat: 날짜 형식 변환 유틸 함수

---
+++ client/src/utils/convertDateToString.ts
@@ -0,0 +1,4 @@
+export const convertYYYYMM = (year: number, month: number) => String(year) + '-' + String(month);
+
+export const convertYYYYMMDD = (year: number, month: number, date: number) =>
+  String(year) + '-' + String(month) + '-' + String(date);
