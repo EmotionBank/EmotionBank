3cb14fe61cd905e0027546ed9ff051fb67ce49b8
yerimstar <yerimhuh@naver.com>
2023-09-20 17:04:25 +0900

    :sparkles: feat: 거래 상세 내역 응답 DTO 생성

---
+++ business/src/main/java/com/emotionbank/business/api/transaction/dto/GetTransactionDetailDto.java
@@ -0,0 +1,49 @@
+package com.emotionbank.business.api.transaction.dto;
+
+import com.emotionbank.business.domain.transaction.constant.Emoticon;
+import com.emotionbank.business.domain.transaction.constant.TransactionType;
+import com.emotionbank.business.domain.transaction.dto.TransactionDto;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class GetTransactionDetailDto {
+
+	@Getter
+	@NoArgsConstructor(access = AccessLevel.PROTECTED)
+	public static class Response {
+		private Emoticon emoticon;
+		private Long amount;
+		private String date;
+		private String time;
+		private String content;
+		private String accountName;
+		private TransactionType transactionType;
+
+		@Builder
+		public Response(Emoticon emoticon, Long amount, String date, String time, String content,
+			String accountName, TransactionType transactionType) {
+			this.emoticon = emoticon;
+			this.amount = amount;
+			this.date = date;
+			this.time = time;
+			this.content = content;
+			this.accountName = accountName;
+			this.transactionType = transactionType;
+		}
+
+		public static Response of(TransactionDto transactionDto, String accountName) {
+			return Response.builder()
+				.emoticon(transactionDto.getEmoticon())
+				.amount(transactionDto.getAmount())
+				.date(transactionDto.getTransactionTime().toLocalDate().toString())
+				.time(transactionDto.getTransactionTime().toLocalTime().toString())
+				.content(transactionDto.getContent())
+				.accountName(accountName)
+				.transactionType(transactionDto.getTransactionType())
+				.build();
+		}
+	}
+}
