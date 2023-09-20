54cdf2ea2af9e105db2196e21fbc362036f64752
jxixeun <jxixeun@gmail.com>
2023-09-20 12:32:00 +0900

    :sparkles: feat: WebConfig에 XSS 방어 Bean 등록

--- business/src/main/java/com/emotionbank/business/global/config/WebConfig.java
+++ business/src/main/java/com/emotionbank/business/global/config/WebConfig.java
@@ -1,9 +1,13 @@
 package com.emotionbank.business.global.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.navercorp.lucy.security.xss.servletfilter.XssEscapeServletFilter;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -19,5 +23,14 @@ public class WebConfig implements WebMvcConfigurer {
 				HttpMethod.DELETE.name(),
 				HttpMethod.OPTIONS.name()
 			);
+	}
+
+	@Bean
+	public FilterRegistrationBean<XssEscapeServletFilter> filterRegistrationBean() {
+		FilterRegistrationBean<XssEscapeServletFilter> filterRegistration = new FilterRegistrationBean<>();
+		filterRegistration.setFilter(new XssEscapeServletFilter());
+		filterRegistration.setOrder(1);
+		filterRegistration.addUrlPatterns("/*");
+		return filterRegistration;
 	}
 }
