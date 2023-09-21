d3b4f351da11a1139b777f57e7f5723ac149c8df
양정훈 <yjhoon3658@naver.com>
2023-09-21 14:12:35 +0900

    :sparkles: feat: AuthorizationHeader 의 유효성을 검증 할 util 클래스 생성

--- business/src/main/java/com/emotionbank/business/global/error/ErrorCode.java
+++ business/src/main/java/com/emotionbank/business/global/error/ErrorCode.java
@@ -29,7 +29,10 @@ public enum ErrorCode {
 	CALENDAR_NOT_EXIST(HttpStatus.NOT_FOUND, "L-001", "해당 일자에 기록이 존재하지 않습니다."),
 
 	// OAuth
-	NOT_SUPPORT_LOGIN_EXCEPTION(HttpStatus.BAD_REQUEST, "O-001", "지원하지 않는 로그인 방식입니다.");
+	NOT_SUPPORT_LOGIN_EXCEPTION(HttpStatus.BAD_REQUEST, "O-001", "지원하지 않는 로그인 방식입니다."),
+	NOT_EXIST_AUTHORIZATION(HttpStatus.UNAUTHORIZED, "O-002", "Authorization Header가 빈값입니다."),
+	NOT_VALID_BEARER_GRANT_TYPE(HttpStatus.UNAUTHORIZED, "O-003", "인증 타입이 Bearer 타입이 아닙니다."),
+	NOT_ACCESS_TOKEN_TYPE(HttpStatus.UNAUTHORIZED, "O-004", "Access Token이 아닙니다.");
 
 	ErrorCode(HttpStatus httpStatus, String errorCode, String message) {
 		this.httpStatus = httpStatus;
