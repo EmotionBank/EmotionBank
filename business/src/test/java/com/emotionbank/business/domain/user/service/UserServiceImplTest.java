6960ed2a4002f3611b8be0977a4a42f83da34d44
최종명 <lanto12@naver.com>
2023-09-22 12:47:10 +0900

    ✨ feat: 팔로우 언팔로우 테스트 코드

--- business/src/test/java/com/emotionbank/business/domain/user/service/UserServiceImplTest.java
+++ business/src/test/java/com/emotionbank/business/domain/user/service/UserServiceImplTest.java
@@ -5,6 +5,7 @@ import static org.mockito.Mockito.*;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -15,8 +16,11 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
+import com.emotionbank.business.domain.user.dto.FollowDto;
 import com.emotionbank.business.domain.user.dto.UserDto;
+import com.emotionbank.business.domain.user.entity.Follow;
 import com.emotionbank.business.domain.user.entity.User;
+import com.emotionbank.business.domain.user.repository.FollowRepository;
 import com.emotionbank.business.domain.user.repository.UserRepository;
 
 class UserServiceImplTest {
@@ -25,6 +29,8 @@ class UserServiceImplTest {
 	private UserServiceImpl userService;
 	@Mock
 	private UserRepository userRepository;
+	@Mock
+	private FollowRepository followRepository;
 
 	@BeforeEach
 	public void beforeEach() {
@@ -47,7 +53,83 @@ class UserServiceImplTest {
 
 	@Test
 	@DisplayName("유저를 팔로우 한다")
-	void searchNonExistentUser() {
+	void followUser() {
+		User follower = User.builder()
+			.userId(1L)
+			.build();
+
+		User followee = User.builder()
+			.userId(2L)
+			.build();
+
+		when(userRepository.findById(1L)).thenReturn(Optional.of(follower));
+		when(userRepository.findById(2L)).thenReturn(Optional.of(followee));
+
+		when(followRepository.findByFolloweeAndFollower(followee, follower)).thenReturn(Optional.empty());
+
+		when(followRepository.save(any(Follow.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+		FollowDto followDto = FollowDto.builder()
+			.follower(1L)
+			.followee(2L)
+			.build();
+
+		userService.followUser(followDto);
+
+		verify(followRepository, times(1)).save(any(Follow.class));
 
 	}
+
+	@Test
+	@DisplayName("유저를 언팔로우 한다")
+	void unFollowUser() {
+		User follower = User.builder()
+			.userId(1L)
+			.build();
+
+		User followee = User.builder()
+			.userId(2L)
+			.build();
+
+		Follow follow = Follow.builder()
+			.followee(followee)
+			.follower(follower)
+			.build();
+
+		when(userRepository.findById(1L)).thenReturn(Optional.of(follower));
+		when(userRepository.findById(2L)).thenReturn(Optional.of(followee));
+
+		when(followRepository.findByFolloweeAndFollower(followee, follower)).thenReturn(Optional.of(follow));
+
+		when(followRepository.save(any(Follow.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+		FollowDto followDto = FollowDto.builder()
+			.follower(1L)
+			.followee(2L)
+			.build();
+
+		userService.followUser(followDto);
+		verify(followRepository, times(1)).delete(any(Follow.class));
+	}
+
+	// @Test
+	// @DisplayName("팔로잉 목록을 조회한다")
+	// void showFollowingList() {
+	//
+	// 	User follower = User.builder()
+	// 		.userId(1L)
+	// 		.build();
+	//
+	// 	User followee = User.builder()
+	// 		.userId(2L)
+	// 		.build();
+	//
+	// 	Follow.builder()
+	// 		.follower(follower)
+	// 		.followee(followee)
+	// 		.build();
+	//
+	// 	when(userRepository.findById(1L)).thenReturn(Optional.of(follower));
+	// 	when(followRepository.findByFollower(follower)).thenReturn()
+	// }
 }
