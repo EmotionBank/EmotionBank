1b584d33b448b2e01aa09a47d8da72eed972e87a
yerimstar <yerimhuh@naver.com>
2023-09-22 14:01:57 +0900

    :sparkles: feat: 카테고리 생성 dto 생성

---
+++ business/src/main/java/com/emotionbank/business/domain/category/dto/CategoryDto.java
@@ -0,0 +1,41 @@
+package com.emotionbank.business.domain.category.dto;
+
+import com.emotionbank.business.api.category.dto.CreateCategoryDto;
+import com.emotionbank.business.domain.category.entity.Category;
+import com.emotionbank.business.domain.transaction.constant.Visibility;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class CategoryDto {
+	private Long categoryId;
+	private Long userId;
+	private String categoryName;
+	private Visibility visibility;
+
+	@Builder
+	public CategoryDto(Long categoryId, Long userId, String categoryName, Visibility visibility) {
+		this.categoryId = categoryId;
+		this.userId = userId;
+		this.categoryName = categoryName;
+		this.visibility = visibility;
+	}
+
+	public static CategoryDto from(CreateCategoryDto.Request request) {
+		return CategoryDto.builder()
+			.userId(request.getUserId())
+			.categoryName(request.getCategoryName())
+			.visibility(Visibility.valueOf(request.getVisibility()))
+			.build();
+	}
+
+	public static CategoryDto from(Category category) {
+		return CategoryDto.builder()
+			.categoryId(category.getCategoryId())
+			.categoryName(category.getCategoryName())
+			.userId(category.getUser().getUserId())
+			.visibility(category.getVisibility())
+			.build();
+	}
+}
