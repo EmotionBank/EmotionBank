6d158da5e390953c0a369aec2889e7d7414e4b0e
yerimstar <yerimhuh@naver.com>
2023-09-22 14:02:33 +0900

    :sparkles: feat: 카테고리 공개 여부 칼럼 추가, 변환 메서드 추가

--- business/src/main/java/com/emotionbank/business/domain/category/entity/Category.java
+++ business/src/main/java/com/emotionbank/business/domain/category/entity/Category.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -11,28 +13,29 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
+import com.emotionbank.business.domain.category.dto.CategoryDto;
+import com.emotionbank.business.domain.transaction.constant.Visibility;
 import com.emotionbank.business.domain.transaction.entity.Transaction;
 import com.emotionbank.business.domain.user.entity.User;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Builder
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
 public class Category {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "category_id")
 	private Long categoryId;
 
 	@ManyToOne
 	@JoinColumn(name = "user_id")
 	private User user;
+
+	@Enumerated(value = EnumType.STRING)
+	private Visibility visibility;
 
 	@OneToMany(mappedBy = "category")
 	private List<Transaction> transactions;
@@ -40,4 +43,20 @@ public class Category {
 	@Column(name = "category_name")
 	private String categoryName;
 
+	@Builder
+	public Category(Long categoryId, User user, Visibility visibility,
+		String categoryName) {
+		this.categoryId = categoryId;
+		this.user = user;
+		this.visibility = visibility;
+		this.categoryName = categoryName;
+	}
+
+	public static Category of(CategoryDto categoryDto, User user) {
+		return Category.builder()
+			.categoryName(categoryDto.getCategoryName())
+			.user(user)
+			.visibility(categoryDto.getVisibility())
+			.build();
+	}
 }
