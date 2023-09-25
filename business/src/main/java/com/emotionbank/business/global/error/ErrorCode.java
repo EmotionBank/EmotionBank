3730fb9df623570227216825eb28321208107fa6
양정훈 <yjhoon3658@naver.com>
2023-09-25 13:53:19 +0900

    :art: refactor: Terms ErrorCode 이해하기 쉬운 네이밍으로 변경

--- business/src/main/java/com/emotionbank/business/global/error/ErrorCode.java
+++ business/src/main/java/com/emotionbank/business/global/error/ErrorCode.java
@@ -42,7 +42,7 @@ public enum ErrorCode {
 	TRANSACTION_NOT_EXIST(HttpStatus.NOT_FOUND, "T-001", "일치하는 거래가 없습니다."),
 
 	// Terms
-	NO_TERMS_CREATE_ROLE(HttpStatus.UNAUTHORIZED, "E-001", "약관을 생성할 수 있는 권한이 없습니다.");
+	TERMS_CREATE_UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "E-001", "약관을 생성할 수 있는 권한이 없습니다.");
 
 	ErrorCode(HttpStatus httpStatus, String errorCode, String message) {
 		this.httpStatus = httpStatus;
