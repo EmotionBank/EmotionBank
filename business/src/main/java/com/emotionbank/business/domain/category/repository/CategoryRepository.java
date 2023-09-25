f5869c4b03da662cb01974bd105acd278e050ba2
yerimstar <yerimhuh@naver.com>
2023-09-25 11:58:27 +0900

    :sparkles: feat: user id와 카테고리명으로 카테고리 찾는 메서드 구현

--- business/src/main/java/com/emotionbank/business/domain/category/repository/CategoryRepository.java
+++ business/src/main/java/com/emotionbank/business/domain/category/repository/CategoryRepository.java
@@ -3,9 +3,13 @@ package com.emotionbank.business.domain.category.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.emotionbank.business.domain.category.entity.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 	Optional<Category> findByCategoryId(Long categoryId);
+
+	@Query("SELECT c FROM Category c WHERE c.user.userId = :userId AND c.categoryName = :categoryName")
+	Category findByUserIdAndCategoryName(Long userId, String categoryName);
 }
