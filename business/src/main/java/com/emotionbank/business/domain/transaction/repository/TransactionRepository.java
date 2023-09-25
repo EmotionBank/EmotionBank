d2e54dc684445dff5964784bfd5a000aa58ffa0e
yerimstar <yerimhuh@naver.com>
2023-09-25 15:30:23 +0900

    :sparkles: feat: 계좌, 날짜, 공개 여부로 조회하는 메서드 추가

--- business/src/main/java/com/emotionbank/business/domain/transaction/repository/TransactionRepository.java
+++ business/src/main/java/com/emotionbank/business/domain/transaction/repository/TransactionRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.emotionbank.business.domain.account.entity.Account;
+import com.emotionbank.business.domain.transaction.constant.Visibility;
 import com.emotionbank.business.domain.transaction.entity.Transaction;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
@@ -15,6 +16,13 @@ public interface TransactionRepository extends JpaRepository<Transaction, Long> 
 	List<Transaction> searchTransactionByAccountAndDate(Account account,
 		Date startDate,
 		Date endDate
+	);
+
+	@Query("SELECT t FROM Transaction t WHERE (t.sender = :account OR t.receiver = :account) AND t.visibility = :visibility AND DATE(t.transactionTime) BETWEEN :startDate AND :endDate")
+	List<Transaction> searchTransactionByAccountAndDateAndVisibility(Account account,
+		Date startDate,
+		Date endDate,
+		Visibility visibility
 	);
 
 	Optional<Transaction> findByTransactionId(Long transactionId);
