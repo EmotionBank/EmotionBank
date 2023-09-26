b8f765aca1f94a87f4027a83571b7da17f887ec1
Seokjun7074 <sukjune96@gmail.com>
2023-09-26 09:54:25 +0900

    ✨ feat: 카테고리 생성 로직

--- client/src/components/transaction/TransactionStep/CreateCategoryModal/CreateCategoryModal.tsx
+++ client/src/components/transaction/TransactionStep/CreateCategoryModal/CreateCategoryModal.tsx
@@ -1,28 +1,47 @@
+import { useState } from 'react';
 import * as S from '@/components/transaction/TransactionStep/CreateCategoryModal/CreateCategoryModal.style';
+import { postCategoryRequestType } from '@/apis/category/postCategory';
 
 const CreateCategoryModal = () => {
+  const initForm = {
+    userId: 'userId',
+    categoryName: '',
+    visibility: 'PUBLIC',
+  };
+  const [categoryForm, setCategoryForm] = useState<postCategoryRequestType>(initForm);
+
+  const handleInputCategory = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setCategoryForm({ ...categoryForm, [name]: value });
+  };
+
+  const handleCreateCategory = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
   return (
     <S.CreateCategoryModalWrapper>
-      <S.CategoryForm>
+      <S.CategoryForm onSubmit={handleCreateCategory} id="createCategory">
         <S.LabelContainer>
           <span>카테고리명 입력</span>
         </S.LabelContainer>
-        <S.CategoryInput />
+        <S.CategoryInput type="text" name="categoryName" onChange={handleInputCategory} />
         <S.LabelContainer>
           <span>공개 여부</span>
         </S.LabelContainer>
         <S.RadioContainer>
           <label>
-            <input type="radio" name="visibility" checked />
+            <input type="radio" name="visibility" value="PUBLIC" defaultChecked onChange={handleInputCategory} />
             공개
           </label>
           <label>
-            <input type="radio" name="visibility" />
+            <input type="radio" name="visibility" value="PRIVATE" onChange={handleInputCategory} />
             비공개
           </label>
         </S.RadioContainer>
       </S.CategoryForm>
-      <S.CreateButton>생성하기</S.CreateButton>
+      <S.CreateButton type="submit" form="createCategory">
+        생성하기
+      </S.CreateButton>
     </S.CreateCategoryModalWrapper>
   );
 };
