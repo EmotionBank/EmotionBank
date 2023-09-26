9ca031095c88cabe723e83295de7ee09e9d3ef20
양정훈 <yjhoon3658@naver.com>
2023-09-26 15:27:18 +0900

    :sparkles: feat: 유저가 약관에 동의 시 데이터를 Update 할 수 있는 Service단 메서드 구현

---
+++ business/src/main/java/com/emotionbank/business/domain/agreement/service/AgreementService.java
@@ -0,0 +1,7 @@
+package com.emotionbank.business.domain.agreement.service;
+
+import com.emotionbank.business.domain.agreement.dto.AgreementDto;
+
+public interface AgreementService {
+	AgreementDto updateAgreement(AgreementDto agreementDto);
+}
