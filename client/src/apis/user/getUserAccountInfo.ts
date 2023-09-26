1103f16641e3e1176be29ff0d8492e58f884e2cf
Seokjun7074 <sukjune96@gmail.com>
2023-09-26 12:38:17 +0900

    ✨ faet: 내 정보 상세조회 API

---
+++ client/src/apis/user/getUserAccountInfo.ts
@@ -0,0 +1,14 @@
+import { axiosInstance } from '@/apis';
+
+export interface getUserAccountInfoType {
+  nickname: string;
+  accountNumber: string;
+  balance: number;
+  following: number;
+  follower: number;
+}
+
+export const getUserAccountInfo = async () => {
+  const { data } = await axiosInstance.get<getUserAccountInfoType>('/users/info/me');
+  return data;
+};
