ad07f32d15036830199e02d6af8d1d90fa6f9c7e
양정훈 <yjhoon3658@naver.com>
2023-09-27 13:00:33 +0900

    :sparkles: feat: 약관 동의가 완료 되었는지 확인 후 권한을 부여하는 Interceptor 구현

---
+++ business/src/main/java/com/emotionbank/business/global/component/AgreementInterceptor.java
@@ -0,0 +1,54 @@
+package com.emotionbank.business.global.component;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import com.emotionbank.business.domain.agreement.constant.State;
+import com.emotionbank.business.domain.agreement.dto.AgreementDto;
+import com.emotionbank.business.domain.agreement.service.AgreementService;
+import com.emotionbank.business.domain.auth.service.JwtManager;
+import com.emotionbank.business.domain.terms.constant.Mandatory;
+import com.emotionbank.business.global.error.ErrorCode;
+import com.emotionbank.business.global.error.exception.BusinessException;
+
+import io.jsonwebtoken.Claims;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class AgreementInterceptor implements HandlerInterceptor {
+	private static final String authorization = "Authorization";
+	private final JwtManager jwtManager;
+	private final AgreementService agreementService;
+
+	@SuppressWarnings("checkstyle:OperatorWrap")
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+		String authorizationHeader = request.getHeader(authorization);
+
+		String token = authorizationHeader.split(" ")[1];
+
+		Claims claims = jwtManager.getTokenClaims(token);
+		Long userId = Long.valueOf((Integer)claims.get("userId"));
+
+		List<AgreementDto> agreementList = agreementService.getAgreementList(userId);
+
+		Long agreementsSize = agreementList
+			.stream()
+			.filter(agreementDto ->
+				agreementDto.getMandatory().equals(Mandatory.ESSENTIAL.name())
+					&& !agreementDto.getState().equals(State.ACTIVE.name())
+			).count();
+
+		if (!agreementsSize.equals(0L)) {
+			throw new BusinessException(ErrorCode.TERMS_NOT_ACTIVE);
+		}
+
+		return true;
+	}
+}
