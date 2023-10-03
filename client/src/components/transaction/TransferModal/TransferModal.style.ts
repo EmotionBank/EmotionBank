b52e9359eaf03ecc509e857e1a87b6add32efd9e
이석준 <sukjune96@gmail.com>
2023-10-03 19:33:14 +0900

    ✨ feat: 송금 양식 입력

--- client/src/components/transaction/TransferModal/TransferModal.style.ts
+++ client/src/components/transaction/TransferModal/TransferModal.style.ts
@@ -1,18 +1,24 @@
+import { Button } from '@/components/common/Button/Button';
 import { Flex } from '@/components/common/Flex/Flex';
 import { float } from '@/constants/animation';
 import styled, { css } from 'styled-components';
 
 export const TransferModalWrapper = styled(Flex)`
+  padding: 2rem;
   width: 30rem;
+  flex-direction: column;
+  gap: 2rem;
 `;
-
+export const TransferModalTitle = styled.h3`
+  color: ${({ theme }) => theme.color.darkgray};
+`;
 export const EmotionContainer = styled(Flex)`
   width: 100%;
   justify-content: space-around;
 `;
 
 export const EmotionImageContainer = styled(Flex)<{ $clicked: boolean }>`
-  min-width: 10rem;
+  min-width: 8rem;
   flex-direction: column;
   justify-content: space-between;
   padding: 1rem;
@@ -26,4 +32,21 @@ export const EmotionImageContainer = styled(Flex)<{ $clicked: boolean }>`
           ${float} 0.7s ease-in-out infinite
         `
       : 'none'};
+`;
+
+export const AmountInput = styled.input`
+  width: 90%;
+  background-color: ${({ theme }) => theme.color.secondary};
+  padding: 1rem;
+  border-radius: 16px;
+  &::placeholder {
+    font-size: 1.4rem;
+  }
+`;
+
+export const SubmitButton = styled(Button)`
+  width: 90%;
+  background-color: ${({ theme }) => theme.color.navy};
+  color: ${({ theme }) => theme.color.white};
+  font-weight: bold;
 `;
--- client/src/components/transaction/TransferModal/TransferModal.tsx
+++ client/src/components/transaction/TransferModal/TransferModal.tsx
@@ -2,12 +2,17 @@ import { useState } from 'react';
 import * as S from '@/components/transaction/TransferModal/TransferModal.style';
 import { positiveEmotionImageList } from '@/constants/emotions';
 import { setEmotionName } from '@/utils/setEmotionName';
+import { replaceDash, setMoneyRegex } from '@/utils/regex';
 
 const TransferModal = () => {
-  const [selectedEmotion, setSelectedEmotion] = useState('');
+  const [selectedEmotion, setSelectedEmotion] = useState('0');
+  const [amount, setAmount] = useState('');
+
+  const handleSubmit = () => {};
 
   return (
     <S.TransferModalWrapper>
+      <S.TransferModalTitle>전달할 감정을 선택하세요!</S.TransferModalTitle>
       <S.EmotionContainer>
         {Object.entries(positiveEmotionImageList).map(([key, value]) => (
           <S.EmotionImageContainer key={key} onClick={() => setSelectedEmotion(key)} $clicked={key === selectedEmotion}>
@@ -16,6 +21,13 @@ const TransferModal = () => {
           </S.EmotionImageContainer>
         ))}
       </S.EmotionContainer>
+      <S.AmountInput
+        type="text"
+        placeholder="전달할 금액을 입력하세요!"
+        value={setMoneyRegex(amount)}
+        onChange={e => setAmount(setMoneyRegex(e.target.value))}
+      />
+      <S.SubmitButton onClick={handleSubmit}>송금하기</S.SubmitButton>
     </S.TransferModalWrapper>
   );
 };
