34e2d7c3a2e18140d444764c967a1d1592de02f0
kghyeong16 <abcd5ksw@naver.com>
2023-10-03 19:12:43 +0900

    :art: refactor:드롭다운 밖 페이지 클릭 시 닫히도록 수정

--- client/src/components/Dropdown/Dropdown.style.ts
+++ client/src/components/Dropdown/Dropdown.style.ts
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 
-export const DropdownWrapper = styled.ul`
+export const DropdownWrapper = styled.div`
+
+`;
+export const DropdownButton = styled.button``;
+export const DropdownContent = styled.div<{ isOpen: boolean }>`
   width: 121px;
   height: auto;
   padding: 6px;
@@ -12,8 +16,12 @@ export const DropdownWrapper = styled.ul`
   box-shadow: 0px 4px 4px 0px color(display-p3 0 0 0 / 0.25);
   list-style-type: none;
   z-index: 1;
+  position: absolute;
+  top: 60%;
+  left: 3%;
+  display: ${props => (props.isOpen ? 'block' : 'none')};
+  /* Other styling for the dropdown content */
 `;
-
 export const DropdownItem = styled.li`
   margin-top: 6px;
   cursor: pointer;
