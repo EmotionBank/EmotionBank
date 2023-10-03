58ef7672b30f38c730bd17f2a28e4eb262c5c5cc
김지은 <jxixeun@gmail.com>
2023-10-03 20:35:01 +0900

    :sparkles: feat: Emoticon에 감정 추가

--- business/src/main/java/com/emotionbank/business/domain/transaction/constant/Emoticon.java
+++ business/src/main/java/com/emotionbank/business/domain/transaction/constant/Emoticon.java
@@ -4,7 +4,15 @@ import lombok.Getter;
 
 @Getter
 public enum Emoticon {
-	HAPPY("기쁨");
+	HAPPY("기쁨"),
+	EXCITING("신남"),
+	FLUTTER("설렘"),
+	SOSO("그저그럼"),
+	SAD("슬픔"),
+	TIRED("피곤"),
+	ANNOYING("짜증"),
+	ANGRY("화남"),
+	GLOOMY("우울");
 
 	private final String message;
 
