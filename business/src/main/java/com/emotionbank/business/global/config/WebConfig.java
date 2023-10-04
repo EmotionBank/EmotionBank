f5302b519b3a3d563499233eb96085ad067ec1a8
양정훈 <yjhoon3658@naver.com>
2023-10-04 13:50:00 +0900

    :ambulance: hotfix: cors origin 변경

--- business/src/main/java/com/emotionbank/business/global/config/WebConfig.java
+++ business/src/main/java/com/emotionbank/business/global/config/WebConfig.java
@@ -34,7 +34,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("http://localhost:3000/")
+			.allowedOrigins("https://emotion-bank.vercel.app/")
 			.allowedMethods(
 				HttpMethod.GET.name(),
 				HttpMethod.POST.name(),
